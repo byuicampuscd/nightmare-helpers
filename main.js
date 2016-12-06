64f49bf939eca4ace73819490c3a0b1127d0ddaa
Joshua McKinney <mckinneyj@byui.edu>
2016-12-05 18:37:47 -0700

    added setWaitTimeout need to add documentation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-modules.export = function (Nightmare) {
-
+module.exports = function (Nightmare) {
+    //this is what the use function does so I figured it would work
     Nightmare.prototype.waitURL = function (url) {
         this.wait(function (url) {
             url = new RegExp(url);
@@ -9,4 +9,19 @@ modules.export = function (Nightmare) {
         return this;
 
     }
+
+
+
+    Nightmare.action("setWaitTimeout", function (min, sec, ms, done) {
+
+        min = min * 60 * 1000;
+        sec = sec * 1000;
+        var timeOut = min + sec + ms;
+        this.options.waitTimeout = timeOut;
+
+        console.log('Set wait timeout to:', timeOut);
+
+        done();
+
+    });
 }
