c221c6ceeb49f173b91c815a520a0a63623a05ec
Joshua McKinney <mckinneyj@byui.edu>
2016-12-05 18:43:54 -0700

    added some comments

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 module.exports = function (Nightmare) {
-    //this is what the use function does so I figured it would work
+    //this is what the nightmare.use function does so I figured it would work and it does
     Nightmare.prototype.waitURL = function (url) {
+        //it cues up the wait for us
         this.wait(function (url) {
             url = new RegExp(url);
             return url.test(document.location.href);
         }, url);
 
+        //and keeps going
         return this;
-
     }
-
-
 
     Nightmare.action("setWaitTimeout", function (min, sec, ms, done) {
 
@@ -25,3 +24,4 @@ module.exports = function (Nightmare) {
 
     });
 }
+di
