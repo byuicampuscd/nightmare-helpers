9d2628775d88784cb8d82b5b1dae5f873010c9e5
Joshua McKinney <mckinneyj@byui.edu>
2016-12-06 15:04:58 -0700

    added gotoLocal

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* eslint-env node, browser */
+/* eslint no-console:0 */
+
 module.exports = function (Nightmare) {
     //this is what the nightmare.use function does so I figured it would work and it does
     Nightmare.prototype.waitURL = function (url) {
@@ -11,6 +14,7 @@ module.exports = function (Nightmare) {
         return this;
     }
 
+    //This allows you to change the wait time out on the fly
     Nightmare.action("setWaitTimeout", function (min, sec, ms, done) {
 
         min = min * 60 * 1000;
@@ -21,7 +25,40 @@ module.exports = function (Nightmare) {
         console.log('Set wait timeout to:', timeOut);
 
         done();
-
     });
+
+    //This resolves a local path to an absolute path on the local system and adds the file protocol
+    Nightmare.prototype.gotoLocal = function (pathText) {
+        var path = require('path');
+        if (pathText.search(/https?/) > -1) {
+            throw new Error("gotoLocal is used to load local files not web addresses.");
+        }
+
+        // preppend file protocol and make absolute path
+        this.goto("file:///" + path.resolve(pathText));
+
+        //and keep going
+        return this;
+    }
+
+    /* This is an example of how to pass in and recive data
+    Nightmare.action('printJAM', function (name, options, parent, win, renderer, done) {
+        console.log(name);
+        parent.respondTo('printJAM', function (words, done) {
+            //document.querySelector('h1').innerHTML = words;
+            console.log("electron scope: " + words);
+            done(null, words);
+
+        });
+        done();
+    },
+    function (words, done) {
+        console.log("words1: " + words);
+        //console.log(this);
+        this.child.call('printJAM', words, done);
+        return this;
+    }
+);
+    
+    */
 }
-//di
